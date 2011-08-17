448e1d44c0368f8eb27cc18752315a535ddaa751
Patrick Thomas <j4kp07@gmail.com>
2011-08-16 23:13:53 -0500

    add main CSS and 404/error templates

---
+++ 404.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<head>
+
+	<title></title>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<meta http-equiv="Content-Style-Type" content="text/css" />
+	<meta http-equiv="Content-Script-Type" content="text/javascript" />
+	
+	<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+	<link rel="icon" href="/favicon.ico" type="image/x-icon" />
+
+	<link rel="stylesheet" type="text/css" href="/css/reset.css" media="screen" />
+	<link rel="stylesheet" type="text/css" href="/css/screen.css" media="screen" />
+	<link rel="stylesheet" type="text/css" href="/css/alerts.css" media="screen" />
+	<link rel="stylesheet" type="text/css" href="/css/forms.css" media="screen" />
+	<link rel="stylesheet" type="text/css" href="/css/lightbox.css" media="screen" />
+	<link rel="stylesheet" type="text/css" href="/css/messages.css" media="screen" />
+	<link rel="stylesheet" type="text/css" href="/css/print.css" media="print" />
+	
+	<script type="text/javascript" src="/js/jquery-1.6.2.min.js"></script>
+	<script type="text/javascript" src="/js/jquery.colorbox.js"></script>
+	<script type="text/javascript" src="/js/jquery.bt.min.js"></script>
+	<script type="text/javascript" src="/js/jquery.form.js"></script>
+	<script type="text/javascript" src="/js/cufon-1.09.js"></script>
+	<script type="text/javascript" src="/js/common.js"></script>
+	
+	<script src="/fonts/Ideal_Sans_300.font.js" type="text/javascript"></script>
+	
+	<script type="text/javascript" charset="utf-8"> Home.init(); </script>
+	
+	<!-- www.phpied.com/conditional-comments-block-downloads/ -->
+	<!--[if IE]><![endif]-->
+
+	<!--[if (gte IE 6)&(lte IE 8)]>
+		<link rel="stylesheet" type="text/css" href="/css/ie.css" />
+		<script type="text/javascript" src="/js/selectivizr.js"></script>
+	<![endif]-->
+	
+	<!-- Always force latest IE rendering engine (even in intranet) & Chrome Frame -->
+	<!--[if IE]><meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"><![endif]-->
+
+</head>
+
+<body>
+	
+	<?php include(DIRNAME . 'templates/header.php') ?>
+	
+	<!-- INSERT WRAPPER AND CONTENT HERE -->
+	
+	<?php include(DIRNAME . 'templates/footer.php') ?>
+	
+</body>
+</html>
