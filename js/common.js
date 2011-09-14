d7c4711cea2d88da4bb133dbe227605f72a7a8c2
Patrick Thomas <j4kp07@gmail.com>
2011-09-14 05:46:17 -0500

    update default common.JS with changeMailTo function

--- js/common.js
+++ js/common.js
@@ -92,19 +92,12 @@ var Home = {
 	init: function() {
 		jQuery(function($) {
 			
-
 		});
 	}
 };
-			
+
 var Site = {
-	
-	config: {
-		base_url: '/',
-		site_url: '/', 
-		upload_dir: '/useruploads/images/'
-	},
-	
+
 	init: function() {
 		
 		jQuery(function($) {
@@ -142,6 +135,7 @@ var Site = {
 				});
 			});
 			
+			
 			$('input[type=button]').click(function() {
 				var el = $(this);
 				location.href = el.attr('rel');
@@ -150,12 +144,17 @@ var Site = {
 			$("#jump_menu").change(function(e) {
 	            window.location.href = $(this).val();
 	        });
-	  
+			
+			var changeMailTo = function(){
+				var mArr = ["@","smashup","luca",".it"];
+				$("#email").attr("href","mailto:"+mArr[2]+mArr[0]+mArr[1]+mArr[3]);
+			}
+					
+			setTimeout(changeMailTo,500);
+			
 			var preload = [
-				'/images/site/indexslide1.jpg', 
-				'/images/site/indexslide2.jpg', 
-				'/images/site/indexslide3.jpg', 
-				'/images/site/indexslide4.jpg'
+				'/images/foobar_01.jpg', 
+				'/images/foobar_02.jpg'
 			];
 			
 			// $.preloadImages(preload);
@@ -166,95 +165,14 @@ var Site = {
 			$('.lightbox').open_colorbox();
 			$('.select').after('<div class="select_skin"></div>');
 			
-			Cufon.replace('h1',{ textShadow: '1px 1px #000'});
-			Cufon.replace('h2',{ textShadow: '1px 1px #000'});
+			Cufon.replace('h2, nav ul li, .info-box b', { fontFamily: 'GeosansLight', hover: 'true' });
+			Cufon.replace('#head-list li, .info-box strong', { fontFamily: 'Myriad Pro', hover: 'true' });
+			Cufon.replace('#banners li#ban-1 a', { fontFamily: 'Myriad Pro', hover: 'true' });
+			Cufon.replace('#banners li#ban-2 a', { fontFamily: 'Myriad Pro', hover: 'true' });
+			Cufon.replace('#banners li#ban-3 a', { fontFamily: 'Myriad Pro', hover: 'true' });
 			
-			var $oe_menu		= $('#oe_menu');
-			var $oe_menu_items	= $oe_menu.children('li');
-			var $oe_overlay		= $('#oe_overlay');
-
-			$oe_menu_items.bind('mouseenter',function(){
-				var $this = $(this);
-				$this.addClass('slided selected');
-				$this.children('div').css('z-index','9999').stop(true,true).slideDown(200,function(){
-					$oe_menu_items.not('.slided').children('div').hide();
-					$this.removeClass('slided');
-				});
-			}).bind('mouseleave',function(){
-				var $this = $(this);
-				$this.removeClass('selected').children('div').css('z-index','1');
-			});
-
-			$oe_menu.bind('mouseenter',function(){
-				var $this = $(this);
-				$oe_overlay.stop(true,true).fadeTo(200, 0.6);
-				$this.addClass('hovered');
-			}).bind('mouseleave',function(){
-				var $this = $(this);
-				$this.removeClass('hovered');
-				$oe_overlay.stop(true,true).fadeTo(200, 0);
-				$oe_menu_items.children('div').hide();
-			})
-			
-			var read_button = function(class_names) {
-		    var r = {
-		      selected: false,
-		      type: 0
-		    };
-
-		    for (var i=0; i < class_names.length; i++) {
-				if (class_names[i].indexOf('selected-') == 0) {
-		        	r.selected = true;
-		      	}
-		      	if (class_names[i].indexOf('segment-') == 0) {
-		        	r.segment = class_names[i].split('-')[1];
-		      	}
-		    };
-		    	return r;
-		  	};
-
-		  	var determine_sort = function($buttons) {
-		    	var $selected = $buttons.parent().filter('[class*="selected-"]');
-		    	return $selected.find('a').attr('data-value');
-		  	};
-
-		  	var determine_kind = function($buttons) {
-		    	var $selected = $buttons.parent().filter('[class*="selected-"]');
-		    	return $selected.find('a').attr('data-value');
-		  	};
-
-		  	var $preferences = {
-		    	duration: 800,
-		    	easing: 'easeInOutQuad',
-		    	adjustHeight: 'dynamic'
-		  	};
-
-		  	var $list = $('#list');
-		  	var $data = $list.clone();
-
-		  	var $controls = $('ul.splitter ul');
-
-		  }); 
+			Cufon.now();
 		});
-	},
-	
-	helpers: {
-		/*
-			TODO This isn't intelligent enough to accept escaped delimiters.  Accepts: "name=value,name2=value2"
-		*/
-		attr_params: function(value) {
-			var param_pairs = value.split(',');
-			var params = {};
-			$.each(param_pairs, function(k, v) {
-				var param_pair = v.split('=');
-				if (param_pair.length == 2)
-				{
-					params[param_pair[0]] = param_pair[1];
-				}
-			});
-			
-			return params;
-		}
 	}
 };
 
