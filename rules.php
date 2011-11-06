fa82217e7e6ff8c803288d811b2472b07ccf1c08
Patrick Thomas <j4kp07@gmail.com>
2011-11-05 22:43:12 -0500

    add preview and redirect rules submission

---
+++ rules.php
@@ -0,0 +1,103 @@
+<?php 
+	$page_title ='&raquo; Set Rules &amp; Options';
+	require_once realpath(dirname(__FILE__)) . '/lib/core/master.inc.php'; 
+	
+    if(!$Auth->loggedIn()) redirect('/login.php');
+	
+	if(!empty($_POST['action']))
+    {
+		if($Error->ok())
+		{
+			$q = new Question($_POST['question_id']);
+			$q->description = $_POST['description'];
+			$q->required = set_default($_POST['required'], 0);
+			$q->update();
+
+			redirect('/preview.php?&id=' . $_POST['question_id']);
+		}
+    }
+	
+	$type = Matrix::fetch("SELECT * FROM matrix WHERE main = 1 ORDER BY weight");
+	$question = new Question($_GET['id']);
+	
+	require_once realpath(dirname(__FILE__)) . '/_header.php'; 
+	echo '<body>';
+	require_once realpath(dirname(__FILE__)) . '/_navigation.php';
+?>
+
+	<div class="wrapper">
+		<div class="container">
+			<div class="row">
+				<div class="span16">
+				
+					<?php if (strlen($Error)): ?>
+						<div class="row">
+							<div style="width: 562px; margin: 0 auto;">
+								<div class="alert-message error">
+							    <p><?= $Error; ?></p>
+							    </div>
+							</div>
+						</div>
+					<?php endif ?>
+						
+					<div class="row">
+						<div style="background-image: url('/assets/images/account/background_acct.jpg'); width: 562px; height: 482px; margin: 20px auto;">
+							<div class="span5" style="padding: 20px 0 0 10px;">
+								<div class="page-header">
+									<h4>Rules &amp; Options</h4>
+								</div>
+								<form action="<?= $_SERVER['PHP_SELF']; ?>" method="post" class="form-stacked" style="margin-left: -20px;"> 
+									<input type="hidden" name="question_id" value="<?= set_default($_GET['id']) ?>">
+									<input type="hidden" name="action" value="true">
+									
+									<div class="clearfix">
+										<label for="description">Brief description of this question:</label> 
+										<textarea rows="3" name="description" id="description" class=""></textarea><br />
+										<span class="help-inline">For internal use only</span>
+									</div>
+									
+									<div class="clearfix">
+										<label id="optionsCheckboxes">Other options</label>
+										<div class="input">
+											<ul class="inputs-list">
+												<li>
+													<label>
+														<input type="checkbox" value="1" name="required">
+														<span>Required &mdash; user must choose one option</span>
+													</label>
+												</li>
+												<li>
+													<label>
+														<input type="checkbox" value="1" name="optionsCheckboxes">
+														<span>Include an option for "None of the above"</span>
+													</label>
+												</li>
+											</ul>
+										</div>
+									</div>
+									
+									<input type="submit" value="Next &raquo;" class="btn primary" />
+									
+									<input type="hidden" name="r" value="<?PHP echo htmlspecialchars(@$_REQUEST['r']); ?>" id="r">
+									
+									<div id="moreOptions"></div>
+									<ol id="viewOptions"></ol>
+								</form>
+							</div>
+							
+							<div class="span3" style="padding: 20px 0 0 30px;">
+								<img src="/assets/images/origami/folder.png" width="100%"><br /><br />
+								<div class="alert-message block-message warning">
+									<strong>What's next?</strong><br />
+									Preview your question and add more.
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+</body>
+</html>
