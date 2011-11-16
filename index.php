04132d81d3023e2a99f32c9cb513f072cbcdb8cb
Patrick Thomas <j4kp07@gmail.com>
2011-11-15 23:06:01 -0600

    add bootstrap to front controller

--- index.php
+++ index.php
@@ -2,55 +2,10 @@
 	ini_set('error_reporting', E_ALL);
 	ini_set('display_errors', 1);	
 
-	// Application flag
-	define('SPF', true);
-
-	// https://twitter.com/#!/marcoarment/status/59089853433921537
 	date_default_timezone_set('America/Los_Angeles');
-
-	// Determine our absolute document root, application and library paths
-	define('DOC_ROOT', realpath(dirname(__FILE__)));
-	define('DIR_SYS',  DOC_ROOT . '/lib/system');
-	define('DIR_CTRL', DOC_ROOT . '/controller');
-	define('DIR_PAGE', DOC_ROOT . '/pages');
-	define('DIR_VIEW', DOC_ROOT . '/views');
-	define('LIBRARY',  DOC_ROOT . '/lib/core');
 	
-	// Global include files
-	require LIBRARY . '/class.debug.php';    // debug...
-	require LIBRARY . '/functions.inc.php';  // spl_autoload_register() is contained in this file
-	require LIBRARY . '/class.dbobject.php'; // DBOBject...
-	require LIBRARY . '/class.objects.php';  // and its subclasses
-
-	// Fix magic quotes
-	if(get_magic_quotes_gpc())
-	{
-		$_POST    = fix_slashes($_POST);
-		$_GET     = fix_slashes($_GET);
-		$_REQUEST = fix_slashes($_REQUEST);
-		$_COOKIE  = fix_slashes($_COOKIE);
-	}
+	require_once realpath(dirname(__FILE__)) . '/bootstrap.php'; 
 	
-	// Load our config settings
-	$config = Config::getConfig();
-
-	// Store session info in the database?
-	if(Config::get('useDBSessions') === true)
-		DBSession::register();
-
-	// Initialize our session
-	session_name('spfs');
-	session_start();
-
-	// Initialize current user
-	$Auth = Auth::getAuth();
-
-	// If you need to bootstrap a first user into the database, you can run this line once
-	// Auth::createNewUser('username', 'password');
-
-	// Object for tracking and displaying error messages
-	$Error = Error::getError();
-
 	$ru = &$_SERVER['REQUEST_URI'];
 	$qmp = strpos($ru, '?');
 	list($path, $params) = $qmp === FALSE
@@ -64,22 +19,34 @@
 	include DIR_SYS.'/router.php';
 	include DIR_SYS.'/config.routes.php';
 		
-	$appname =  DIR_CTRL . '/'; // Symbolic application links
-	$filename = DOC_ROOT . $_SERVER['REQUEST_URI'] . '.php'; // Load filename directly
-	$pagename = DIR_PAGE . rtrim($_SERVER['REQUEST_URI'], '/') . '.md'; // MARKDOWN templates, for future use
-	$requestURI = explode('/', $_SERVER['REQUEST_URI']);
+	$appname =  DIR_CTRL . '/'; // Load symbolic application links
+	$indexname =  DIR_CTRL . '/'; // Load symbolic index links
+	$scriptname = DOC_ROOT . $_SERVER['REQUEST_URI'] . '.php'; // Load script directly
+	$pagename = DIR_PAGE . rtrim($_SERVER['REQUEST_URI'], '/') . '.md'; // Load MARKDOWN templates
 		
-	for($i= 1; $i < sizeof($requestURI) - 1; $i++)	
+	// Load app controller
+	for($i = 1; $i < sizeof($requestURI) - 1; $i++)	
 	{
 		$appname .= $requestURI[$i] . '/'; 
 		if (file_exists(rtrim($appname, '/') . '.php')) 
 		{
-			include rtrim($appname, '/') . '.php'; // Load controller
+			include rtrim($appname, '/') . '.php'; 
 			exit();
 		}
 	}
 	
-	if(file_exists($pagename))
+	// If app not found, load index controller based on URI count
+	for($i = 1; $i < sizeof($requestURI) - 1; $i++)
+	{
+		$indexname .= $requestURI[$i] . '/'; 
+		if (file_exists($indexname . 'index.php') && $i == (sizeof($requestURI) -2)) 
+		{
+			include $indexname . 'index.php'; 
+			exit();
+		}
+	}
+	
+	if(file_exists($pagename)) // Load MARKDOWN page
 	{
 		include_once DIR_SYS . '/markdown.php';
 		$str = file_get_contents($pagename);
@@ -103,9 +70,9 @@
 HTML;
 		exit();
 	}
-	else if(file_exists($filename))
+	else if(file_exists($scriptname))
 	{
-		require $filename; // Load file directly
+		require $scriptname; // Load script directly
 		exit();
 	}
 	else if($ctrl = Router::controller())
